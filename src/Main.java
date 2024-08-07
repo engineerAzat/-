7625c9ef20393dcfc1f07473eb83af2f63f919c6
Azat <vildus@yandex.ru>
2024-08-07 23:53:39 +0300

    07082024

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        SecurityUtis.check("asdqewd231", "12346_", "12346_");
+        try {
+            SecurityUtis.check("asdqewd231", "12346_*", "12346_");
+
+        } catch (WrongPasswordException e) {
+            System.err.println("Error!");
+        }
 
 
     }
