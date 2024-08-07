4f2f8dbadf20ae4d44d47ce6e1debd3c76a41c86
Azat <vildus@yandex.ru>
2024-08-08 00:02:40 +0300

    07082024

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            SecurityUtis.check("asdqewd231", "12346_*", "12346_");
+            SecurityUtis.check("asdqewd231", "12346_", "12346_");
             System.out.println("Все отлично");
         }catch (Exception e) {
             System.out.println("Error!" + e.getMessage());
