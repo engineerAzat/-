d7b9094d9b9bcb912406535947741059d81cc5e5
Azat <vildus@yandex.ru>
2024-09-01 11:01:53 +0300

    07082024

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,14 @@ public class Main {
 
         try {
             SecurityUtis.check("asdqewd231", "12346_", "12346_");
+
             System.out.println("Все отлично");
-        }catch (Exception e) {
-            System.out.println("Error!" + e.getMessage());
+        } catch (WrongLoginException e) {
+            System.err.println(e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            System.out.println("Finally!");
         }
 
 
